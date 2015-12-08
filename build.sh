6bbe7ad139307fd069b50faf64c77a3b7c56dbff
yutaka <shigure@refy.net>
2015-12-08 08:07:45 +0000

    add name check

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ NAME=$1
 PORT=$(($RANDOM + 3000))
 DOMAIN=onsen.tech
 
+if [ "x$NAME" = "x" ]; then
+  echo "[ERROR] name not found"
+  exit 1
+fi
+
 # remove current container & image
 echo 'stopping...'
 docker stop ${NAME}
