3ee2c0281e9cc200f5aa2777143b57bf8ba34775
yutaka <shigure@refy.net>
2016-01-06 12:56:08 +0000

    remove jquery dom code

--- minamo-server/public/js/console.js
+++ minamo-server/public/js/console.js
@@ -3,12 +3,10 @@ function rootDomain(){
 }
 
 function removeHandler(name){
-  return function(){
-    if(window.confirm('This container permanently removed. Are you sure?')){
-      $.get('/api/destroy', {'service': name, 't': Date.now()}, function(){
-        updateStatus();
-      });
-    }
+  if(window.confirm('This container permanently removed. Are you sure?')){
+    $.get('/api/destroy', {'service': name, 't': Date.now()}, function(){
+      updateStatus();
+    });
   }
 }
 
@@ -69,122 +67,31 @@ function isStopping(text){
 }
 
 function toLabelColor(text){
-  if(isRunning(text)) return 'label-success';
-  if(isPrepareing(text)) return 'label-info';
-  if(isStopping(text)) return 'label-warning';
-  if(isStopped(text)) return 'label-danger';
+  if(isRunning(text)) return 'success';
+  if(isPrepareing(text)) return 'info';
+  if(isStopping(text)) return 'warning';
+  if(isStopped(text)) return 'danger';
 }
 
 function startContainer(name){
-  return function(){
-    $.get('/api/start', {'service': name, 't': Date.now()}, function(){
-      updateStatus();
-    });
-    return false;
-  }
+  $.get('/api/start', {'service': name, 't': Date.now()}, function(){
+    updateStatus();
+  });
 }
 
 function stopContainer(name){
-  return function(){
-    $.get('/api/stop', {'service': name, 't': Date.now()}, function(){
-      updateStatus();
-    });
-    return false;
-  }
+  $.get('/api/stop', {'service': name, 't': Date.now()}, function(){
+    updateStatus();
+  });
 }
 
 function restartContainer(name){
-  return function(){
-    $.get('/api/restart', {'service': name, 't': Date.now()}, function(){
-      updateStatus();
-    });
-    return false;
-  }
-}
-
-function actionButton(name, status){
-  var dom = new Dom();
-  var action = $('<div></div>').addClass('btn-group');
-  var actions = $('<ul></ul>').addClass('dropdown-menu');
-  action
-    .append($('<button></button>', {'data-toggle':'dropdown', 'class':'btn dropdown-toggle'}).text('Action ')
-      .append($('<span>').addClass('caret')))
-    .append(actions);
-  if(isRunning(status)){
-    actions
-      .append($('<li>').append(dom.a('stop', '#', stopContainer(name))))
-      .append($('<li>').append(dom.a('restart', '#', restartContainer(name))))
-  }else if(isStopped(status)){
-    actions
-      .append($('<li>').append(dom.a('start', '#', startContainer(name))))
-  }
-  return action;
-}
-
-function updateStatus(){
-  $.get('/api/status', {'t': Date.now()}, function(json){
-    var dom = new Dom();
-    var table = $('<table></table>').addClass('table table-hover')
-      .append($('<tr></td>')
-        .append(dom.th('name'))
-        .append(dom.th('status'))
-        .append(dom.th('head'))
-        .append(dom.th('uptime'))
-        .append(dom.th('repo'))
-        .append(dom.th('action'))
-        .append(dom.th('remove'))
-    );
-    var div = $('#statuses_xs');div.children().remove();
-    var keys = Object.keys(json);
-    // build non xs
-    for(var i = 0; i < keys.length; ++i){
-      var cont = json[keys[i]];
-      var status = cont.status;
-      var created = cont.created ? new Date(cont.created).toLocaleString() : "";
-      var repo = 'http://git.' + rootDomain() + '/' + keys[i] + '.git';
-      if(cont.repo === 'external'){
-        repo = 'http://' + rootDomain() + '/api/hooks/' + keys[i] + '?key=' + json[keys[i]].key;
-        cont.head = 'external';
-      }
-      table.append($('<tr>')
-        .append($('<td>').append(dom.a(keys[i], '//' + keys[i] + '.' + rootDomain())))
-        .append($('<td>').append($('<span class="label" />').text(status).addClass(toLabelColor(status))))
-        .append($('<td>').addClass(cont.repo === 'external' ? 'text-muted' : '').text(cont.head))
-        .append($('<td>').append($('<span>',{'data-toggle':'tooltip',title:created}).text(cont.uptime).tooltip()))
-        .append($('<td>').append($('<input>').addClass('form-control').val(repo)))
-        .append($('<td>').append(actionButton(keys[i], status)))
-        .append($('<td>').append($('<button>', {'class': 'btn btn-danger'}).text('remove').click(removeHandler(keys[i]))))
-      );
-      // xs panel
-      var panel = $('<div class="panel-group"></div>');
-      var panel2 = $('<div class="panel panel-default"></div>');
-      panel.append(panel2);
-      panel2.append($('<div class="panel-heading" data-toggle="collapse"></div>').attr('href', '#svc_' + keys[i])
-        .append($('<div class="row" data-toggle="collapse"></div>')
-          .append($('<div class="col-xs-6"></div>')
-            .append($('<h4 class="panel-title visible-xs-inline"></h4>').text(keys[i])))
-          .append($('<div class="col-xs-6 text-right"></div>')
-            .append($('<p class="label vcenter text-right"></p>').text(status).addClass(toLabelColor(status))))));
-      panel2.append($('<div class="panel-collapse collapse"></div>').collapse('hide').attr("id",'svc_'+keys[i])
-        .append($('<div class="panel-body"></div>')
-          .append($('<dl class="dl-horizontal"></dl>')
-            .append($('<dt>').text('service'))
-            .append($('<dd>').append(dom.a(keys[i] + '.' + rootDomain(), '//' + keys[i] + '.' + rootDomain())))
-            .append($('<dt>').text('head'))
-            .append($('<dd>').addClass(cont.repo === 'external' ? 'text-muted' : '').text(cont.head))
-            .append($('<dt>').text('uptime'))
-            .append($('<dd>').append($('<span>',{'data-toggle':'tooltip',title:created}).text(cont.uptime).tooltip()))
-            .append($('<dt>').text('repo'))
-            .append($('<dd>').append($('<input>').addClass('form-control').val('http://git.' + rootDomain() + '/' + keys[i] + '.git'))))
-          .append(actionButton(keys[i], status))
-          .append(' ')
-          .append($('<button>', {'class': 'btn btn-danger'}).text('remove').click(removeHandler(keys[i])))));
-      div.append(panel);
-    }
-    $('#statuses').children().remove();
-    $('#statuses').append(table);
+  $.get('/api/restart', {'service': name, 't': Date.now()}, function(){
+    updateStatus();
   });
 }
+
+function updateStatus(){ }
 
 function load(){
   updateStatus();
