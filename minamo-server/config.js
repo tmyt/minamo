cf73cb926b0b58c043fd73ed70da064ebb1f76d1
yutaka <shigure@refy.net>
2016-08-03 04:23:06 +0000

    Fix env name typo

--- minamo-server/config.js
+++ minamo-server/config.js
@@ -5,5 +5,5 @@ let path = require('path');
 module.exports = {
     proto: "https",
     domain: 'minamo.io',
-    repo_path: process.env.MINAMO_PORES_PATH || path.resolve('repos')
+    repo_path: process.env.MINAMO_REPOS_PATH || path.resolve('repos')
 };
