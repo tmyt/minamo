211f6246d92f974b75b65d4c5a922c5ff6b5004f
yutaka <shigure@refy.net>
2016-08-05 10:56:03 +0900

    improve log stream interface

--- minamo-server/public/js/logstream.js
+++ minamo-server/public/js/logstream.js
@@ -1,7 +1,12 @@
 var log = document.getElementById('log');
 var socket = io();
 socket.on('line', function(line){
+  // check scroll position
+  var tail = log.scrollHeight <= log.scrollTop + log.clientHeight;
+  // append element
   var span = document.createElement('span');
   span.innerHTML = line;
   log.appendChild(span.firstChild);
+  // scroll if needed
+  if(tail) log.scrollTop = log.scrollHeight;
 });
