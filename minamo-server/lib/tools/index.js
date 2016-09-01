f500f1634d3d734e3539331bd6ba9983b1decb93
yutaka <shigure@refy.net>
2016-09-01 19:01:09 +0900

    fix redis server would not installed

--- minamo-server/lib/tools/index.js
+++ minamo-server/lib/tools/index.js
@@ -8,19 +8,24 @@ const path = require('path')
 
 class Tools{
 
+  getRequiredPackages(extraEnv){
+    let extraPackages = {};
+    let packages = (extraEnv['MINAMO_REQUIRED_PACKAGES'] || '').split(',');
+    for(let i = 0; i < packages.length; ++i){
+      if(!packages[i].trim()) continue;
+      extraPackages['MINAMO_BUILD_REQUIRED_' + packages[i].toUpperCase()] = 'true';
+    }
+    return extraPackages;
+  }
+
   build(repo){
     let extraEnv = fs.readJsonSync(path.join(config.repo_path, repo) + '.env');
     let envString = '';
     let envKeys = Object.keys(extraEnv);
-    let extraPackages = {};
+    let extraPackages = this.getRequiredPackages(extraEnv);
     for(let i = 0; i < envKeys.length; ++i){
       if(envKeys[i] === 'MINAMO_REQUIRED_PACKAGES') continue;
       envString += `ENV ${envKeys[i]} ${extraEnv[envKeys[i]]}\n`;
-    }
-    let packages = (extraEnv['REQUIRED_PACKAGES'] || '').split(',');
-    for(let i = 0; i < packages.length; ++i){
-      if(!packages[i].trim()) continue;
-      extraPackages['MINAMO_BUILD_REQUIRED_' + packages[i].toUpperCase()] = 'true';
     }
     let env = Object.assign({'DOMAIN': config.domain, 'EXTRAENV': envString}, extraPackages);
     exec(path.join(__dirname, 'build.sh') + ' ' + repo, {env: env});
