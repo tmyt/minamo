c34cd767888601aa1f45f669a72ab6226c253544
yutaka <shigure@refy.net>
2016-10-07 17:34:07 +0900

    generate css,js from sass,babel by gulp

---
+++ minamo-server/gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const gulp = require('gulp')
+    , sass = require('gulp-sass')
+    , concatCss = require('gulp-concat-css')
+    , cleanCss = require('gulp-clean-css')
+    , babel = require('gulp-babel');
+
+const paths = {
+  'scss': 'src/css/',
+  'css': 'public/css/',
+  'babel': 'src/babel/',
+  'js': 'public/babel/'
+};
+
+gulp.task('scss', () => {
+  return gulp.src(paths.scss + '**/*.scss')
+    .pipe(sass())
+    .pipe(concatCss('minamo.min.css'))
+    .pipe(cleanCss())
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('babel', () => {
+  return gulp.src(paths.babel + '**/*.babel')
+    .pipe(babel({
+      presets: ['react', 'es2015'],
+      minified: true,
+      comments: false
+    }))
+    .pipe(gulp.dest(paths.js));
+});
+
+gulp.task('build', ['babel', 'scss']);
