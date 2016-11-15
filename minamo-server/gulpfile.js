1617e599166df8a495e152b5a300396a77452f8a
yutaka <shigure@refy.net>
2016-11-15 12:01:46 +0900

    merge console.js and console.babel

--- minamo-server/gulpfile.js
+++ minamo-server/gulpfile.js
@@ -2,6 +2,7 @@
 
 const gulp = require('gulp')
     , sass = require('gulp-sass')
+    , concat = require('gulp-concat')
     , concatCss = require('gulp-concat-css')
     , cleanCss = require('gulp-clean-css')
     , babel = require('gulp-babel');
@@ -10,11 +11,12 @@ const paths = {
   'scss': 'src/css/',
   'css': 'public/css/',
   'babel': 'src/babel/',
-  'js': 'public/babel/'
+  'js': 'public/js/'
 };
 
 gulp.task('scss', () => {
-  return gulp.src([paths.scss + '**/*.scss', 'public/components/Umi/dist/css/bootstrap.css'])
+  return gulp.src([paths.scss + '**/*.scss', 'public/components/Umi/dist/css/bootstrap.css'],
+    {base: 'public/css'})
     .pipe(sass())
     .pipe(concatCss('minamo.min.css'))
     .pipe(cleanCss())
@@ -22,12 +24,13 @@ gulp.task('scss', () => {
 });
 
 gulp.task('babel', () => {
-  return gulp.src(paths.babel + '**/*.babel')
+  return gulp.src([paths.babel + '**/*.babel', 'src/js/console.js'])
     .pipe(babel({
       presets: ['react', 'es2015'],
       minified: true,
       comments: false
     }))
+    .pipe(concat('console.js'))
     .pipe(gulp.dest(paths.js));
 });
 
