3d5d65d6964f56b1be364618c3a633ef66b84896
yutaka <shigure@refy.net>
2016-11-30 18:24:11 +0900

    modulerize console.babel

--- minamo-server/src/babel/console/api.js
+++ minamo-server/src/babel/console/api.js
@@ -1,161 +1,161 @@
-function removeHandler(name){
-  if(window.confirm('This container permanently removed. Are you sure?')){
-    $.get('/api/destroy', {'service': name, 't': Date.now()}, function(){ });
-  }
-}
+'use strict';
 
-function createNew(){
-  var name = $('#service_name').val();
-  var template = $('#template').val();
-  var external = $('#external_repo').val();
-  if(!name.match(/^[a-z][a-z0-9-]*[a-z]$/)){
-    $('#errmsg').text('error: service name should be [a-z][a-z0-9-]*[a-z]');
+const ContainerRegexpString = '[a-z][a-z0-9-]*[a-z]';
+const ContainerRegexp = new RegExp(`^${ContainerRegexpString}\$`);
+
+const Internal = (function(){
+  const nameFlags = ['has-error', 'has-success'];
+  const glyphFlags = ['glyphicon-remove', 'glyphicon-ok'];
+
+  function call(api, name, cb){
+    $.get(`/api/${api}`, {'service': name, 't': Date.now()}, cb || function(){});
+  }
+
+  function clearValidation(){
+    $('#service_name_group').removeClass(nameFlags.join(' '));
+    $('#service_name_glyph').removeClass(glyphFlags.join(' '));
+  }
+  function clearForms(){
+    $('#service_name').val('');
+    $('#external_repo').val('');
+  }
+  function createNew(){
+    let name = $('#service_name').val();
+    let template = $('#template').val();
+    let external = $('#external_repo').val();
+    if(!name.match(ContainerRegexp)){
+      $('#errmsg').text(`error: service name should be ${ContainerRegexpString}`);
+      return false;
+    }
+    $.ajax({
+      type: 'GET',
+      url: '/api/create',
+      data: {'service': name, 'template': template, 'external': external, 't': Date.now()},
+      success: () => {
+        clearValidation();
+        clearForms();
+        showToast(`Service "${name}" created`, 'success');
+      },
+      error: function(request){
+        showToast(`Service "${name}" creation failed. ${request.responseText}`, 'warning');
+      }
+    });
     return false;
   }
-  $.ajax({
-    type: 'GET',
-    url: '/api/create',
-    data: {'service': name, 'template': template, 'external': external, 't': Date.now()},
-    success: function(){
-      $('#service_name_group').removeClass('has-success').removeClass('has-error');
-      $('#service_name_glyph').removeClass('glyphicon-ok').removeClass('glyphicon-remove');
-      $('#service_name').val("");
-      $('#external_repo').val("");
-      showToast('', 'Service "' + name + '" created', 'success');
-    },
-    error: function(request){
-      showToast('', 'Service "' + name + '" creation failed. ' + request.responseText, 'warning');
-    }
-  });
-  return false;
-}
 
-function updateCredentials(){
-  $.ajax({
-    type: 'POST',
-    url: '/api/credentials/update',
-    data: {'password': $('#password').val()},
-    success: function(){
-      showToast('', 'Credential update successful', 'success');
-      $('#password').val('');
-    },
-    error: function(){
-      showToast('', 'Credential update failed', 'warning');
-    }
-  });
-  return false;
-}
-
-function showToast(title, message, kind){
-  toastr[kind](message, title);
-}
-
-function isRunning(text){
-  return text === 'running';
-}
-
-function isStopped(text){
-  return text === 'stopped' || isExited(text);
-}
-
-function isPrepareing(text){
-  return text === 'prepareing';
-}
-
-function isStopping(text){
-  return text === 'stopping';
-}
-
-function isExited(text){
-  return text === 'exited';
-}
-
-function toLabelColor(text){
-  if(isExited(text)) return 'primary';
-  if(isRunning(text)) return 'success';
-  if(isPrepareing(text)) return 'info';
-  if(isStopping(text)) return 'warning';
-  if(isStopped(text)) return 'danger';
-}
-
-function handleServiceAction(key, name){
-  switch(key){
-    case "config":
-      showEnvConfig(name);
-      break;
-    case "logs":
-      showLogs(name);
-      break;
-    case "start":
-      startContainer(name);
-      break;
-    case "stop":
-      stopContainer(name);
-      break;
-    case "restart":
-      restartContainer(name);
-      break;
+  function updateCredentials(){
+    $.ajax({
+      type: 'POST',
+      url: '/api/credentials/update',
+      data: {'password': $('#password').val()},
+      success: function(){
+        showToast('Credential update successful', 'success');
+        $('#password').val('');
+      },
+      error: function(){
+        showToast('Credential update failed', 'warning');
+      }
+    });
+    return false;
   }
-}
 
-function startContainer(name){
-  $.get('/api/start', {'service': name, 't': Date.now()}, function(){ });
-}
+  function showToast(title, message, kind){
+    if(kind === undefined){ kind = message; message = title; title = ''; }
+    toastr[kind](message, title);
+  }
 
-function stopContainer(name){
-  $.get('/api/stop', {'service': name, 't': Date.now()}, function(){ });
-}
+  function createAddNewButton(){
+    let addnew = $('<tr id="envlist_addnew"><td colspan="3"><button class="btn btn-default form-control">add new</button>');
+    addnew.find('button').click(function(){
+      $('#envlist_addnew').remove();
+      $('#envlist').append(createEnvRow()).append(createAddNewButton());
+      return false;
+    });
+    return addnew;
+  }
 
-function restartContainer(name){
-  $.get('/api/restart', {'service': name, 't': Date.now()}, function(){ });
-}
+  function createEnvRow(name, value){
+    let row = $('<tr data-env="env-item"/>');
+    row.append($('<td><input class="form-control" name="env-name" /></td>'));
+    row.append($('<td><input class="form-control" name="env-value" /></td>'));
+    row.append($('<td><button class="btn btn-danger form-control"><span class="glyphicon glyphicon-trash" /></button></td>'));
+    row.find('input[name="env-name"]').val(name || '');
+    row.find('input[name="env-value"]').val(value || '');
+    row.find('button').click(()=>{ row.remove(); return false; });
+    return row;
+  }
+
+  function setClassForFlag(element, flagName, trueOrFalse){
+    element.removeClass(flagName[+!trueOrFalse])
+      .addClass(flagName[+!!trueOrFalse]);
+  }
+
+  function load(){
+    $('#newform').submit(createNew);
+    $('#credentialform').submit(updateCredentials);
+    $('#service_name').keyup(function(){
+      if($('#service_name').val() === ''){
+        clearValidation();
+      }else{
+        let state = $('#service_name').val().match(ContainerRegexp);
+        setClassForFlag($('#service_name_group'), nameFlags, state);
+        setClassForFlag($('#service_name_glyph'), glyphFlags, state);
+      }
+    });
+    $('#external_repo').keyup(function(){
+      $('#template').prop('disabled', $('#external_repo').val() !== '');
+    });
+    $('#env_save').click(function(){
+      let items = $('#envlist tr[data-env="env-item"]').toArray();
+      let env = items.map(i => [
+          $(i).find('input[name="env-name"]').val(),
+          $(i).find('input[name="env-value"]').val()
+        ])
+        .reduce((pr, cr) => { pr[cr[0]] = cr[1]; return pr; }, {});
+      $.ajax({
+        type: 'POST',
+        url: '/api/env/update',
+        data: {'env': JSON.stringify(env), 'service': $('#target_service').val()},
+        success: () => showToast('Env updated!', 'success'),
+        error: () => showToast('Env update failed', 'warning')
+      });
+    });
+  }
+
+  $(function(){
+    // congirue toastr
+    toastr.options.closeButton = true;
+    toastr.options.progressBar = true;
+    // load components
+    load();
+  })
+
+  return {
+    'call': call,
+    'createEnvRow': createEnvRow,
+    'createAddNewButton': createAddNewButton
+  };
+})();
 
 function showEnvConfig(name){
   // clear
   $('#envlist').children().remove();
   $('#envlist').append($('<thead><tr><th>name</th><th>value</th></tr></thead>'));
   $('#target_service').val(name);
-  $.get('/api/env', {'service': name, 't': Date.now()}, function(json){
-    var env = JSON.parse(json);
+  Internal.call('env', name, json=>{
+    let env = JSON.parse(json);
     // add trailing button
-    var keys = Object.keys(env);
-    for(var i = 0; i < keys.length; ++i){
-      $('#envlist').append(createEnvRow(keys[i], env[keys[i]]));
-    }
-    $('#envlist').append(createAddNewButton());
+    Object.keys(env).reduce((pr, cr) => pr.append(Internal.createEnvRow(cr, env[cr])), $('#envlist'));
+    $('#envlist').append(Internal.createAddNewButton());
     $('#envconfig_frame').modal();
   });
 }
 
 function showLogs(name){
-  $.get('/api/logs', {'service': name, 't': Date.now()}, function(logs){
+  Internal.call('logs', name, logs=>{
     $('#logs').val(logs);
     $('#logs_frame').modal();
   });
-}
-
-function createAddNewButton(){
-  var addnew = $('<tr id="envlist_addnew"><td colspan="3"><button class="btn btn-default form-control">add new</button>');
-  addnew.find('button').click(function(){
-    $('#envlist_addnew').remove();
-    $('#envlist').append(createEnvRow()).append(createAddNewButton());
-    return false;
-  });
-  return addnew;
-}
-
-function createEnvRow(name, value){
-  var row = $('<tr data-env="env-item"/>');
-  row.append($('<td><input class="form-control" name="env-name" /></td>'));
-  row.append($('<td><input class="form-control" name="env-value" /></td>'));
-  row.append($('<td><button class="btn btn-danger form-control"><span class="glyphicon glyphicon-trash" /></button></td>'));
-  row.find('input[name="env-name"]').val(name || '');
-  row.find('input[name="env-value"]').val(value || '');
-  row.find('button').click(function(){
-    row.remove();
-    return false;
-  })
-  return row;
 }
 
 function initializeSocket(cb){
@@ -164,52 +164,3 @@ function initializeSocket(cb){
   return cookie => socket.emit('fetch', cookie);
 }
 
-function load(){
-  $('#newform').submit(createNew);
-  $('#credentialform').submit(updateCredentials);
-  $('#service_name').keyup(function(){
-    if($('#service_name').val() === ""){
-      $('#service_name_group').removeClass('has-success').removeClass('has-error');
-      $('#service_name_glyph').removeClass('glyphicon-ok').removeClass('glyphicon-remove');
-    }else if($('#service_name').val().match(/^[a-z][a-z0-9-]*[a-z]$/)){
-      $('#service_name_group').addClass('has-success').removeClass('has-error');
-      $('#service_name_glyph').addClass('glyphicon-ok').removeClass('glyphicon-remove');
-    }else{
-      $('#service_name_group').addClass('has-error').removeClass('has-success');
-      $('#service_name_glyph').addClass('glyphicon-remove').removeClass('glyphicon-ok');
-    }
-  });
-  $('#external_repo').keyup(function(){
-    if($('#external_repo').val() === ""){
-      $('#template').prop('disabled', false);
-    }else{
-      $('#template').prop('disabled', true);
-    }
-  });
-  $('#env_save').click(function(){
-    var items = $('#envlist tr[data-env="env-item"]');
-    var env = {};
-    for(var i = 0 ; i < items.length; ++i){
-      var name = items.eq(i).find('input[name="env-name"]').val();
-      var value = items.eq(i).find('input[name="env-value"]').val();
-      env[name] = value;
-    }
-    $.ajax({
-      type: 'POST',
-      url: '/api/env/update',
-      data: {'env': JSON.stringify(env), 'service': $('#target_service').val()},
-      success: function(){ },
-      error: function(){
-        showToast('', 'Env update failed', 'warning');
-      }
-    });
-  });
-}
-
-$(document).ready(function(){
-  // congirue toastr
-  toastr.options.closeButton = true;
-  toastr.options.progressBar = true;
-  // load components
-  load();
-})
