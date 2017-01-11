dc40f02f5da61ba684b43bcbe1cf85b5c8b6b37e
yutaka <shigure@refy.net>
2017-01-11 10:36:30 +0900

    render logstream as xterm

--- minamo-server/api/logstream.js
+++ minamo-server/api/logstream.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const readline = require('readline')
-    , a2h = require('ansi2html-extended')
     , spawn = require('child_process').spawn;
 
 function tailf(path){
@@ -14,68 +13,11 @@ function tailf(path){
   return rl;
 }
 
-function stylize(str){
-  if(!str) return '<span class="ansi_console_snippet">&#8203;</span>';
-  return a2h.fromString({wrapped: true}, str)
-}
-
-function getStyle(str){
-  let re = /(\033\[(\d+)(;\d+)?m)/gm;
-  let res;
-  let state = {color:'', bold:'', italic: ''};
-  while((res = re.exec(str)) != null){
-    switch(res[2]){
-      case '1':
-        state.bold = true;
-        break;
-      case '22':
-        state.bold = false;
-        break;
-      case '3':
-        state.italic = true;
-        break;
-      case '23':
-        state.italic = false;
-        break;
-      case undefined:
-      case '0':
-      case '39':
-        state.color = '';
-        break;
-      case '30':
-      case '31':
-      case '32':
-      case '33':
-      case '34':
-      case '35':
-      case '36':
-      case '37':
-      case '90':
-      case '91':
-      case '92':
-      case '93':
-      case '94':
-      case '95':
-      case '96':
-      case '97':
-        state.color = res[2];
-        if(res[3]) state.color += ';' + res[3];
-        break;
-    }
-  }
-  return state;
-}
-
 module.exports = function(io){
   io.of('/log').on('connection', socket => {
     let tail = tailf('/tmp/minamo/build.log');
-    let state = {color:'', bold:'', italic: ''};
     tail.on('line', line => {
-      if(state.bold) line = "\u001b[1m" + line;
-      if(state.italic) line = "\u001b[3m" + line;
-      if(!!state.color) line = "\u001b[" + state.color + "m" + line;
-      state = getStyle(line);
-      socket.emit('line', stylize(line))
+      socket.emit('data', `${line}\r\n`);
     });
     socket.on('disconnect', () => tail.kill());
   });
--- minamo-server/src/babel/logstream/app.js
+++ minamo-server/src/babel/logstream/app.js
@@ -1,16 +1,8 @@
 const log = document.getElementById('log');
 const socket = io('/log');
-socket.on('line', function(line){
-  // check scroll position
-  const tail = log.scrollHeight <= log.scrollTop + log.clientHeight;
-  // append element
-  const span = document.createElement('span');
-  span.innerHTML = line;
-  log.appendChild(span.firstChild);
-  // remove overflow log
-  while(log.children.length > 5000){
-    log.removeChild(log.children[0]);
-  }
-  // scroll if needed
-  if(tail) log.scrollTop = log.scrollHeight;
+const term = new Terminal(80, 30);
+term.open(log);
+term.fit();
+socket.on('data', line => {
+  term.write(line);
 });
