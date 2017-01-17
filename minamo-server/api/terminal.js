b718445880e4acb3d98c691c477d826c7310426b
yutaka <shigure@refy.net>
2017-01-17 10:14:15 +0900

    run bash like as login shell

--- minamo-server/api/terminal.js
+++ minamo-server/api/terminal.js
@@ -4,14 +4,20 @@ const pty = require('pty.js');
 
 module.exports = function(io){
   io.of('/term').on('connection', socket => {
-    let term = pty.spawn('bash', [], {
-      name: 'xterm-256color',
+    let term = pty.spawn('bash', ['-l'], {
+      name: 'xterm-color',
       cols: 80,
-      rows: 24
+      rows: 24,
+      env: {
+        HOME: process.env.HOME,
+        SHELL: '/bin/bash',
+        USER: process.env.USER,
+        LANG: process.env.LANG
+      }
     });
     socket.emit('init');
     socket.emit('data', '\r\n');
-    term.on('data', d => socket.emit('data', d));
+    term.on('data', d => socket.compress(true).emit('data', d));
     socket.on('data', d => term.write(d));
     socket.on('resize', d => term.resize(d[0], d[1]));
     socket.on('disconnect', () => term.destroy());
