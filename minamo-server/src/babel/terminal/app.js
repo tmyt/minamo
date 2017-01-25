176a35b266af829cff9a6f7430a2035b014a8beb
yutaka <shigure@refy.net>
2017-01-25 11:54:59 +0900

    force replace \r\n to \n to prevent unexpected linebreak

--- minamo-server/src/babel/terminal/app.js
+++ minamo-server/src/babel/terminal/app.js
@@ -8,7 +8,7 @@ $(function(){
   const term = new Terminal(80, 30);
   const socket = io('/term');
   term.open(document.getElementById('terminal'));
-  term.on('data', d => socket.emit('data', d));
+  term.on('data', d => socket.emit('data', d.replace(/\x0D\x0A/g, '\n')));
   term.on('resize', d => socket.emit('resize', [d.cols, d.rows]));
   socket.on('data', d => term.write(d));
   socket.on('init', () => socket.emit('resize', [term.cols, term.rows]));
