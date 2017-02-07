fc16eefdefb70b593d87ff7b895935eb239deda2
yutaka <shigure@refy.net>
2017-02-07 15:11:06 +0900

    optimize webpack performance

--- minamo-server/webpack.config.js
+++ minamo-server/webpack.config.js
@@ -1,9 +1,20 @@
 'use strict';
 
 const webpack = require('webpack')
-    , path = require('path');
+    , path = require('path')
+    , HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 
 let plugins = [
+  new HardSourceWebpackPlugin({
+    cacheDirectory: path.join(process.cwd(), '.cache/webpack/[confighash]'),
+    recordsPath: path.join(process.cwd(), '.cache/webpack/[confighash]/records.json'),
+    configHash: webpackConfig => require('node-object-hash')().hash(webpackConfig),
+    environmentHash: {
+      root: process.cwd(),
+      directories: ['node_modules'],
+      files: ['package.json'],
+    },
+  }),
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
   }),
@@ -15,12 +26,10 @@ let plugins = [
 ];
 
 if(process.env.NODE_ENV === 'production'){
-  plugins = plugins.concat([
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      mangle: true
-    }),
-  ]);
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false },
+    mangle: true
+  }));
 }
 
 module.exports = {
@@ -32,12 +41,23 @@ module.exports = {
   output: {
     path: './public',
     filename: '[name].js',
-    chunkFilename: '[id].bundle.js',
+    chunkFilename: '[id].[hash].bundle.js',
     publicPath: '/'
   },
   module: {
     rules: [
-      { test: /\.js$/, use: [{ loader: 'babel-loader', options: { presets: ['es2015', 'react'], babelrc: false }}] },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [['modern-browsers', {fullSupport: true, modules: false}], 'react'],
+            plugins: ['transform-runtime'],
+            babelrc: false,
+          }
+        }]
+      },
       { test: /\.scss$/, use: ['style-loader', 'css-loader?minimize', 'sass-loader'] },
       { test: /\.css$/, use: ['style-loader', 'css-loader?minimize&-url', 'remove-urlimport-loader'] },
     ]
