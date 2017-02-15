4509e15f33e516e76d31985b4b3d1bda2f0c5eba
yutaka <shigure@refy.net>
2017-02-15 10:29:49 +0900

    refactor: auth library

--- minamo-server/lib/auth/index.js
+++ minamo-server/lib/auth/index.js
@@ -1,31 +1,29 @@
 'use strict';
 
 const express = require('express')
-    , router = express.Router()
     , passport = require('passport');
 
 function authRouter(provider){
+  const authenticate = (req, res, next) => {
+    const redir = req.session.redir || req.query._redir;
+    req.session.redir = redir;
+    passport.authenticate(provider, (err, user, info) => {
+      delete req.session.redir;
+      if(err) { return next(err); }
+      if(!user) { return res.redirect(`/login${redir?`?_redir=${encodeURIComponent(redir)}`:''}`); }
+      req.login(user, err => {
+        if(err) { return next(err); }
+        return res.redirect(redir || '/');
+      });
+    })(req, res, next);
+  };
   const r = express.Router();
-  const opts = { failureRedirect: '/login' };
-  const _next = passport.authenticate(provider, opts);
-  const _start = (req, res, next) => {
-    req.session.redir = req.query._redir;
-    _next(req, res, next);
-  };
-  const _callback = (req, res) => {
-    const redir = req.session.redir;
-    delete req.session.redir;
-    res.redirect(redir || '/');
-  };
-  r.get(`/${provider}/`, _start);
-  r.get(`/${provider}/callback`, _next, _callback);
-  r.post(`/${provider}/`, _start);
-  r.post(`/${provider}/callback`, _start, _callback);
+  r.get(`/${provider}()(/callback)?`, authenticate);
+  r.post(`/${provider}()(/callback)?`, authenticate);
   return r;
 }
 
-router.use('/', authRouter('twitter'));
-router.use('/', authRouter('github'));
-router.use('/', authRouter('local'));
+const router = express.Router()
+router.use('/', authRouter('github'), authRouter('twitter'), authRouter('local'));
 
 module.exports = router;
