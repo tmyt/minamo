3bb7bf5a8be81f465d89987102d30da253fc54bc
yutaka <shigure@refy.net>
2017-05-09 11:08:16 +0900

    enable eslint

---
+++ minamo-server/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "browser": true,
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2,
+      {
+        "SwitchCase": 1,
+        "VariableDeclarator": { "const": 2 },
+        "ArrayExpression": "first"
+      }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single",
+      { "allowTemplateLiterals": true }
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": 1,
+    "no-control-regex": 1,
+    "react/jsx-uses-vars": "error",
+    "react/jsx-uses-react": "error",
+  },
+  "globals": {
+    "$": true,
+    "toastr": true,
+  }
+};
