2b32778744371e7de1c62b7bcedde3e207846d82
yutaka <shigure@refy.net>
2017-05-09 09:27:28 +0900

    set interface name `minamo0` for ISH

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 apt-get install git docker.io nginx lua-nginx-redis make gcc libssh-dev g++
-docker network create --driver bridge shell
+docker network create -d bridge -o 'com.docker.network.bridge.name'='minamo0' shell
