6efc3f9bc39fa3281e869ade68b6e97316c83fb8
yutaka <shigure@refy.net>
2017-05-10 17:01:17 +0900

    add Dockerfile for ISH

---
+++ images/shell/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build $* -t minamo-internal/shell .
--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 apt-get install git docker.io nginx lua-nginx-redis make gcc libssh-dev g++
 docker network create -d bridge -o 'com.docker.network.bridge.name'='minamo0' shell
+./images/shell/build.sh
